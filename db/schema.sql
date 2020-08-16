7c19ef86274506c7d88202626909a38403df6c58
Desert Python <mattsenneff@aol.com>
2020-08-15 20:36:07 -0700

    some installs

--- db/schema.sql
+++ db/schema.sql
@@ -1,52 +1,26 @@
-CREATE TABLE Department (
-    id INTEGER PRIMARY KEY
-    dept_name VARCHAR(30)
-
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+USE employees;
+CREATE TABLE department (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+CREATE TABLE employee (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
 )
-CREATE TABLE emplRole(
-    id INTEGER PRIMARY KEY
-    employee_role VARCHAR(30)
-    salary DECIMAL
-    dept_id INTEGER
-)
-CREATE TABLE employee(
-    id INTEGER PRIMARY KEY
-
-    first_name: VARCHAR(30)
-
-    last_name: VARCHAR(30) to hold employee last name
-
-    role_id: INTEGER to hold reference to employee role
-
-    manager_id: INT to hold reference to another employee that is manager of the current employee. This field may be null if the employee has no manager.
-)
-
-
-
-    -- Department
-
-    --     id: INT PRIMARY KEY
-
-    --     name: VARCHAR(30) to hold department name
-
-    -- Role
-
-    --     id: INT PRIMARY KEY
-
-    --     title: VARCHAR(30) to hold role title
-
-    --     salary: DECIMAL to hold role salary
-
-    --     department_id: INT to hold reference to department role belongs to
-
-    -- Employee
-
-    --     id: INT PRIMARY KEY
-
-    --     first_name: VARCHAR(30) to hold employee first name
-
-    --     last_name: VARCHAR(30) to hold employee last name
-
-    --     role_id: INT to hold reference to employee role
-
-    --     manager_id: INT to hold reference to another employee that is manager of the current employee. This field may be null if the employee has no manager.
