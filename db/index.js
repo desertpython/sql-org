973d439b67a6057857ee10d071eb7a97d7cc8fa6
Desert Python <mattsenneff@aol.com>
2020-10-21 11:51:38 -0700

    find all emp by manager

--- db/index.js
+++ db/index.js
@@ -84,12 +84,13 @@ class DB{
           departmentId
         );
     }
-    
-    
 
-
-    
-    
+    findAllEmployeesByManager(managerId) {
+        return this.connection.promise().query(
+          "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;",
+          managerId
+        );
+    }
     
     
     
