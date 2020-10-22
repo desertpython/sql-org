010f04e5e5efe6c8af7c99800d0accfa6b9e0f2b
Desert Python <mattsenneff@aol.com>
2020-10-21 18:36:54 -0700

    fixing bugs but still promise unhandled

--- db/connection.js
+++ db/connection.js
@@ -2,9 +2,7 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
   host: "localhost",
-  // Your username
   user: "root",
-  // Your password
   password: "Geauxc0de!",
   database: "employees"
 });
